a64c04ed23bea6b4d49f5026bf3667719fce6a5a
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-11-16 09:22:39 +0530

    Update 3254-find-the-power-of-k-size-subarrays-i.cpp

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -1,3 +1,5 @@
+## solution 1
+
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
@@ -23,5 +25,43 @@ public:
         }
 
         return vec;
+    }
+};
+
+## solution 2
+
+
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        if (k == 1) {
+            return nums;
+        }
+        
+        int n = nums.size();
+        vector<int> result;
+        deque<int> window;
+        
+        for (int i = 0; i < n; i++) {
+            // Remove elements outside window
+            while (!window.empty() && i - window.front() >= k) {
+                window.pop_front();
+            }
+            
+            // Check consecutive sequence
+            if (window.empty() || nums[i] - nums[i-1] == 1) {
+                window.push_back(i);
+            } else {
+                window.clear();
+                window.push_back(i);
+            }
+            
+            // Add result when window size is k
+            if (i >= k-1) {
+                result.push_back(window.size() == k ? nums[i] : -1);
+            }
+        }
+        
+        return result;
     }
 };
