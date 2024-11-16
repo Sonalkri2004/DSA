c9ce7ddbba16d8e4412589dbb56580b48ecacf88
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-11-16 09:20:25 +0530

    Time: 3 ms (46.25%), Space: 32.8 MB (91.28%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
-        if (k == 1) {
-            return nums;
-        }
-        
+
         int n = nums.size();
-        vector<int> result;
-        deque<int> window;
-        
-        for (int i = 0; i < n; i++) {
-            // Remove elements outside window
-            while (!window.empty() && i - window.front() >= k) {
-                window.pop_front();
+        vector<int> vec(n - k + 1, -1);
+
+        for(int i = 0; i <= n - k; i++){
+            int v = nums[i];
+            bool isValid = true;
+
+            for(int j = i; j < i + k - 1; j++){
+                if(nums[j] + 1 != nums[j + 1]){
+                    isValid = false;
+                    break;
+                }
+                v = max(v, nums[j + 1]);
             }
-            
-            // Check consecutive sequence
-            if (window.empty() || nums[i] - nums[i-1] == 1) {
-                window.push_back(i);
-            } else {
-                window.clear();
-                window.push_back(i);
-            }
-            
-            // Add result when window size is k
-            if (i >= k-1) {
-                result.push_back(window.size() == k ? nums[i] : -1);
+
+            if(isValid){
+                vec[i] = v;
             }
         }
-        
-        return result;
+
+        return vec;
     }
 };
