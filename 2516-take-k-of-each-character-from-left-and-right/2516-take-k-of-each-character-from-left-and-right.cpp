fb354551cc7b8688349f6468c706c4a3459beb66
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-11-20 09:07:34 +0530

    Time: 11 ms (69.76%), Space: 12.1 MB (56.21%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.cpp
@@ -1,28 +1,46 @@
+// class Solution {
+// public:
+//     int takeCharacters(string s, int k) {
+//         // Total counts
+//         vector<int> count(3, 0);
+//         for (char c : s) {
+//             count[c - 'a']++;
+//         }
+        
+//         if (*min_element(count.begin(), count.end()) < k) {
+//             return -1;
+//         }
+        
+//         // Sliding Window
+//         int res = INT_MAX;
+//         int l = 0;
+//         for (int r = 0; r < s.length(); r++) {
+//             count[s[r] - 'a']--;
+            
+//             while (*min_element(count.begin(), count.end()) < k) {
+//                 count[s[l] - 'a']++;
+//                 l++;
+//             }
+//             res = min(res, static_cast<int>(s.length()) - (r - l + 1));
+//         }
+//         return res;
+//     }
+// };
+
+
 class Solution {
 public:
     int takeCharacters(string s, int k) {
-        // Total counts
-        vector<int> count(3, 0);
-        for (char c : s) {
-            count[c - 'a']++;
-        }
+        vector<int> d(3), h(3);
+        for (char c : s) d[c-'a']++;
+        for (int i = 0; i < 3; i++) if ((d[i] -= k) < 0) return -1;
         
-        if (*min_element(count.begin(), count.end()) < k) {
-            return -1;
-        }
-        
-        // Sliding Window
-        int res = INT_MAX;
-        int l = 0;
+        int m = 0, l = 0;
         for (int r = 0; r < s.length(); r++) {
-            count[s[r] - 'a']--;
-            
-            while (*min_element(count.begin(), count.end()) < k) {
-                count[s[l] - 'a']++;
-                l++;
-            }
-            res = min(res, static_cast<int>(s.length()) - (r - l + 1));
+            h[s[r]-'a']++;
+            while (l <= r && h[s[r]-'a'] > d[s[r]-'a']) h[s[l++]-'a']--;
+            m = max(m, r - l + 1);
         }
-        return res;
+        return s.length() - m;
     }
 };
