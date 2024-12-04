c301bc71792277537374b5a24750634164180407
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-12-04 09:39:26 +0530

    Time: 0 ms (100%), Space: 17.2 MB (7.53%) - LeetHub

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -0,0 +1,16 @@
+// #1 Greedy Two Pointers
+//   Complexity - Time O(n) & Space O(1)
+
+class Solution {
+public:
+    bool canMakeSubsequence(string source, string target) {
+        int targetIdx = 0;
+        int targetLen = target.length();
+        for (char currChar : source) {
+            if (targetIdx < targetLen && (target[targetIdx] - currChar + 26) % 26 <= 1) {
+                targetIdx++;
+            }
+        }
+        return targetIdx == targetLen;
+    }
+};
