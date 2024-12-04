7ec583ae8b6d9345fcdf6ed9b6b2bfe3f9eae26b
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-12-04 09:41:17 +0530

    Time: 0 ms (100%), Space: 17.2 MB (7.53%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,16 +1,42 @@
 // #1 Greedy Two Pointers
 //   Complexity - Time O(n) & Space O(1)
 
+// class Solution {
+// public:
+//     bool canMakeSubsequence(string source, string target) {
+//         int targetIdx = 0;
+//         int targetLen = target.length();
+//         for (char currChar : source) {
+//             if (targetIdx < targetLen && (target[targetIdx] - currChar + 26) % 26 <= 1) {
+//                 targetIdx++;
+//             }
+//         }
+//         return targetIdx == targetLen;
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool canMakeSubsequence(string source, string target) {
-        int targetIdx = 0;
-        int targetLen = target.length();
-        for (char currChar : source) {
-            if (targetIdx < targetLen && (target[targetIdx] - currChar + 26) % 26 <= 1) {
-                targetIdx++;
+        int srcLen = source.size();
+        int tgtLen = target.size();
+        int srcIdx = 0;
+        int tgtIdx = 0;
+        
+        while(srcIdx < srcLen && tgtIdx < tgtLen) {
+            if((source[srcIdx] == target[tgtIdx]) || 
+               (source[srcIdx] == 'z' && target[tgtIdx] == 'a') || 
+               (source[srcIdx] + 1 == target[tgtIdx])) {
+                srcIdx++;
+                tgtIdx++;
+            } else {
+                srcIdx++;
             }
         }
-        return targetIdx == targetLen;
+        return tgtIdx == tgtLen;
     }
 };
+
+// Complexity - Time O(n) & Space O(1)
